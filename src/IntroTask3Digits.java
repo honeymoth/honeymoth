6187e7aaf820670adf72340f5934320acc342468
kozlova_sofia <mothfromhoney@gmail.com>
2021-09-09 17:52:27 +0300

    first commit test

---
+++ src/IntroTask3Digits.java
@@ -0,0 +1,10 @@
+public class IntroTask3Digits {
+    public static void main(String[] args){
+        int x = 1001;
+        if (x > 99 && x < 1000) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}

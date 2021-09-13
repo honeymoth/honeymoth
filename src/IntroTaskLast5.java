8bc5117b7c7132e38376d820184b3dbc760f3ad3
kozlova_sofia <mothfromhoney@gmail.com>
2021-09-13 12:44:27 +0300

    intro tasks

---
+++ src/IntroTaskLast5.java
@@ -0,0 +1,10 @@
+public class IntroTaskLast5 {
+    public static void main(String[] args){
+        int x = 150;
+        if (x%10 == 5){
+            System.out.println(x +" оканчивается на 5");
+        }else {
+            System.out.println(x+" не оканчивается на 5 (");
+        }
+    }
+}
---
+++ src/IntroTaskSameDigits.java
@@ -0,0 +1,10 @@
+public class IntroTaskSameDigits {
+    public static void main(String[] args){
+        int x =120;
+        if (x > 9 && x% 10 == (x / 10)%10){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}

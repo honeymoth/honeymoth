9b61bec197a7f031565d38478db4fbf16906965c
kozlova_sofia <mothfromhoney@gmail.com>
2021-09-14 16:13:24 +0300

    more intro tasks

---
+++ src/IntroTaskCat.java
@@ -0,0 +1,14 @@
+public class IntroTaskCat {
+    public static void main(String[] args){
+        int cat = 2;
+        if (cat % 10 == 1){
+            System.out.println(cat + " кот");
+        }else {
+            if (cat % 10 >1 && cat % 10 <5){
+                System.out.println(cat + " кота");
+            }else {
+                System.out.println(cat + " котов");
+            }
+        }
+    }
+}
---
+++ src/IntroTaskLeap.java
@@ -0,0 +1,14 @@
+public class IntroTaskLeap {
+    public static void main(String[] args){
+        int year = 2021;
+        if (year %  400 == 0){
+            System.out.println(year +" - високосный");
+        }else{
+            if (year % 100 != 0 && year % 4 == 0){
+                System.out.println(year +" - високосный");
+            }else{
+                System.out.println(year +" - не високосный");
+            }
+        }
+    }
+}
